976a35f96b430de8e35b8aa21d0eec6cfbd70834
Shah-Pankit <pankitshah493@gmail.com>
2025-08-07 19:14:21 +0530

    final commit

--- projects/script.js
+++ projects/script.js
@@ -20,11 +20,11 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio Pankit Shah";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
+            document.title = "Thank you for visiting my Portfolio !";
             $("#favicon").attr("href", "/assets/images/favhand.png");
         }
     });
