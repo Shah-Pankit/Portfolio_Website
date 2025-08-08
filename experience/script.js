14c865273414a93994112d3949ab32d41120ecfb
Shah-Pankit <pankitshah493@gmail.com>
2025-08-08 10:54:10 +0530

    Final Changes

--- experience/script.js
+++ experience/script.js
@@ -65,7 +65,7 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
+        document.title = "Experience | Portfolio Pankit Shah";
         $("#favicon").attr("href","/assets/images/favicon.png");
     }
     else {
